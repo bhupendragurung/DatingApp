1433081d26356d206213de4f74fa03596b618f9f
bhupendragurung <bhupendragurung263@gmail.com>
2026-09-21 21:59:38 +0100

    chore: Changes to primary construction style

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
-    public class AppDbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         
     }
