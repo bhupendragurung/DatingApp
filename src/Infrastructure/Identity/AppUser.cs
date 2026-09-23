225d630c9680c6d442c7dc27f7cac5ae46b8644c
bhupendragurung <bhupendragurung263@gmail.com>
2026-09-23 10:57:49 +0100

    feat: add profile fields to AppUser with entity configuration

--- src/Infrastructure/Identity/AppUser.cs
+++ src/Infrastructure/Identity/AppUser.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace Infrastructure.Identity
+namespace Infrastructure.Identity;
+  
+public class AppUser : IdentityUser<Guid>
 {
-   public class AppUser : IdentityUser<Guid>
-{
+    public required string KnownAs { get; set; }
+    public required string Gender { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
-}
+
